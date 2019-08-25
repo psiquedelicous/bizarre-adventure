f0ee719db0f566b5f8f7ffefca87caf805aa91b8
psiquedelicous <54308734+psiquedelicous@users.noreply.github.com>
2019-08-25 16:24:17 +0300

    Create scriptDeliver.js

---
+++ scriptDeliver.js
@@ -0,0 +1,315 @@
+//Tietoraitti 4//
+if (localStorage.Tietoraitti4 === "true")
+{
+  document.getElementById('Tietoraitti4_green').style.display='block';
+}
+else {
+  document.getElementById('Tietoraitti4_green').style.display='none';
+}
+
+button_Tietoraitti4_green.onclick = function functionTietoraitti4_green(){
+  var pic = "green/Tietoraitti4_green";
+  document.getElementById('Tietoraitti4_green').style.display='block';
+  localStorage.Tietoraitti4 = true;
+};
+
+button_Tietoraitti4_red.onclick = function functionTietoraitti4_red(){
+  var pic = "green/Tietoraitti4_green";
+  document.getElementById('Tietoraitti4_green').style.display='none';
+  localStorage.Tietoraitti4 = false;
+};
+
+//Tietoraitti 6//
+
+if (localStorage.Tietoraitti6 === "true")
+{
+  document.getElementById('Tietoraitti6_green').style.display='block';
+}
+else {
+  document.getElementById('Tietoraitti6_green').style.display='none';
+}
+
+button_Tietoraitti6_green.onclick = function functionTietoraitti6_green(){
+  var pic = "green/Tietoraitti6_green";
+  document.getElementById('Tietoraitti6_green').style.display='block';
+  localStorage.Tietoraitti6 = true;
+};
+
+button_Tietoraitti6_red.onclick = function functionTietoraitti6_red(){
+  var pic = "green/Tietoraitti6_green";
+  document.getElementById('Tietoraitti6_green').style.display='none';
+  localStorage.Tietoraitti6 = false;
+};
+
+//Tietoraitti 8//
+if (localStorage.Tietoraitti8 === "true")
+{
+  document.getElementById('Tietoraitti8_green').style.display='block';
+}
+else {
+  document.getElementById('Tietoraitti8_green').style.display='none';
+}
+
+button_Tietoraitti8_green.onclick = function functionTietoraitti8_green(){
+  var pic = "green/Tietoraitti8_green";
+  document.getElementById('Tietoraitti8_green').style.display='block';
+  localStorage.Tietoraitti8 = true;
+};
+
+button_Tietoraitti8_red.onclick = function functionTietoraitti8_red(){
+  var pic = "green/Tietoraitti8_green";
+  document.getElementById('Tietoraitti8_green').style.display='none';
+  localStorage.Tietoraitti8 = false;
+};
+
+//Tietoraitti 10//
+if (localStorage.Tietoraitti10 === "true")
+{
+  document.getElementById('Tietoraitti10_green').style.display='block';
+}
+else {
+  document.getElementById('Tietoraitti10_green').style.display='none';
+}
+
+button_Tietoraitti10_green.onclick = function functionTietoraitti10_green(){
+  var pic = "green/Tietoraitti10_green";
+  document.getElementById('Tietoraitti10_green').style.display='block';
+  localStorage.Tietoraitti10 = true;
+};
+
+button_Tietoraitti10_red.onclick = function functionTietoraitti10_red(){
+  var pic = "green/Tietoraitti10_green";
+  document.getElementById('Tietoraitti10_green').style.display='none';
+  localStorage.Tietoraitti10 = false;
+};
+
+//Tutkijankatu 1//
+if (localStorage.Tutkijankatu1 === "true")
+{
+  document.getElementById('Tutkijankatu1_green').style.display='block';
+}
+else {
+  document.getElementById('Tutkijankatu1_green').style.display='none';
+}
+
+button_Tutkijankatu1_green.onclick = function functionTutkijankatu1_green(){
+  var pic = "green/Tutkijankatu1_green";
+  document.getElementById('Tutkijankatu1_green').style.display='block';
+  localStorage.Tutkijankatu1 = true;
+};
+
+button_Tutkijankatu1_red.onclick = function functionTutkijankatu1_red(){
+  var pic = "green/Tutkijankatu1_green";
+  document.getElementById('Tutkijankatu1_green').style.display='none';
+  localStorage.Tutkijankatu1 = false;
+};
+
+//Tutkijankatu 2//
+if (localStorage.Tutkijankatu2 === "true")
+{
+  document.getElementById('Tutkijankatu2_green').style.display='block';
+}
+else {
+  document.getElementById('Tutkijankatu2_green').style.display='none';
+}
+
+button_Tutkijankatu2_green.onclick = function functionTutkijankatu2_green(){
+  var pic = "green/Tutkijankatu2_green";
+  document.getElementById('Tutkijankatu2_green').style.display='block';
+  localStorage.Tutkijankatu2 = true;
+};
+
+button_Tutkijankatu2_red.onclick = function functionTutkijankatu2_red(){
+  var pic = "green/Tutkijankatu2_green";
+  document.getElementById('Tutkijankatu2_green').style.display='none';
+  localStorage.Tutkijankatu2 = false;
+};
+
+//Tutkijankatu 3//
+if (localStorage.Tutkijankatu3 === "true")
+{
+  document.getElementById('Tutkijankatu3_green').style.display='block';
+}
+else {
+  document.getElementById('Tutkijankatu3_green').style.display='none';
+}
+
+button_Tutkijankatu3_green.onclick = function functionTutkijankatu3_green(){
+  var pic = "green/Tutkijankatu3_green";
+  document.getElementById('Tutkijankatu3_green').style.display='block';
+  localStorage.Tutkijankatu3 = true;
+};
+
+button_Tutkijankatu3_red.onclick = function functionTutkijankatu3_red(){
+  var pic = "green/Tutkijankatu3_green";
+  document.getElementById('Tutkijankatu3_green').style.display='none';
+  localStorage.Tutkijankatu3 = false;
+};
+
+//Tutkijankatu 4//
+if (localStorage.Tutkijankatu4 === "true")
+{
+  document.getElementById('Tutkijankatu4_green').style.display='block';
+}
+else {
+  document.getElementById('Tutkijankatu4_green').style.display='none';
+}
+
+button_Tutkijankatu4_green.onclick = function functionTutkijankatu4_green(){
+  var pic = "green/Tutkijankatu4_green";
+  document.getElementById('Tutkijankatu4_green').style.display='block';
+  localStorage.Tutkijankatu4 = true;
+};
+
+button_Tutkijankatu4_red.onclick = function functionTutkijankatu4_red(){
+  var pic = "green/Tutkijankatu4_green";
+  document.getElementById('Tutkijankatu4_green').style.display='none';
+  localStorage.Tutkijankatu4 = false;
+};
+
+//Tutkijankatu 5//
+if (localStorage.Tutkijankatu5 === "true")
+{
+  document.getElementById('Tutkijankatu5_green').style.display='block';
+}
+else {
+  document.getElementById('Tutkijankatu5_green').style.display='none';
+}
+
+button_Tutkijankatu5_green.onclick = function functionTutkijankatu5_green(){
+  var pic = "green/Tutkijankatu5_green";
+  document.getElementById('Tutkijankatu5_green').style.display='block';
+  localStorage.Tutkijankatu5 = true;
+};
+
+button_Tutkijankatu5_red.onclick = function functionTutkijankatu5_red(){
+  var pic = "green/Tutkijankatu5_green";
+  document.getElementById('Tutkijankatu5_green').style.display='none';
+  localStorage.Tutkijankatu5 = false;
+};
+
+//Tutkijankatu 7A//
+if (localStorage.Tutkijankatu7A === "true")
+{
+  document.getElementById('Tutkijankatu7A_green').style.display='block';
+}
+else {
+  document.getElementById('Tutkijankatu7A_green').style.display='none';
+}
+
+button_Tutkijankatu7A_green.onclick = function functionTutkijankatu7A_green(){
+  var pic = "green/Tutkijankatu7A_green";
+  document.getElementById('Tutkijankatu7A_green').style.display='block';
+  localStorage.Tutkijankatu7A = true;
+};
+
+button_Tutkijankatu7A_red.onclick = function functionTutkijankatu7A_red(){
+  var pic = "green/Tutkijankatu7A_green";
+  document.getElementById('Tutkijankatu7A_green').style.display='none';
+  localStorage.Tutkijankatu7A = false;
+};
+
+//Tutkijankatu 7B//
+if (localStorage.Tutkijankatu7B === "true")
+{
+  document.getElementById('Tutkijankatu7B_green').style.display='block';
+}
+else {
+  document.getElementById('Tutkijankatu7B_green').style.display='none';
+}
+
+button_Tutkijankatu7B_green.onclick = function functionTutkijankatu7B_green(){
+  var pic = "green/Tutkijankatu7B_green";
+  document.getElementById('Tutkijankatu7B_green').style.display='block';
+  localStorage.Tutkijankatu7B = true;
+};
+
+button_Tutkijankatu7B_red.onclick = function functionTutkijankatu7B_red(){
+  var pic = "green/Tutkijankatu7B_green";
+  document.getElementById('Tutkijankatu7B_green').style.display='none';
+  localStorage.Tutkijankatu7B = false;
+};
+
+//Tieteenkatu 5//
+if (localStorage.Tieteenkatu5 === "true")
+{
+  document.getElementById('Tieteenkatu5_green').style.display='block';
+}
+else {
+  document.getElementById('Tieteenkatu5_green').style.display='none';
+}
+
+button_Tieteenkatu5_green.onclick = function functionTieteenkatu5_green(){
+  var pic = "green/Tieteenkatu5_green";
+  document.getElementById('Tieteenkatu5_green').style.display='block';
+  localStorage.Tieteenkatu5 = true;
+};
+
+button_Tieteenkatu5_red.onclick = function functionTieteenkatu5_red(){
+  var pic = "green/Tieteenkatu5_green";
+  document.getElementById('Tieteenkatu5_green').style.display='none';
+  localStorage.Tieteenkatu5 = false;
+};
+
+//Tieteenkatu 3A//
+if (localStorage.Tieteenkatu3A === "true")
+{
+  document.getElementById('Tieteenkatu3A_green').style.display='block';
+}
+else {
+  document.getElementById('Tieteenkatu3A_green').style.display='none';
+}
+
+button_Tieteenkatu3A_green.onclick = function functionTieteenkatu3A_green(){
+  var pic = "green/Tieteenkatu3A_green";
+  document.getElementById('Tieteenkatu3A_green').style.display='block';
+  localStorage.Tieteenkatu3A = true;
+};
+
+button_Tieteenkatu3A_red.onclick = function functionTieteenkatu3A_red(){
+  var pic = "green/Tieteenkatu3A_green";
+  document.getElementById('Tieteenkatu3A_green').style.display='none';
+  localStorage.Tieteenkatu3A = false;
+};
+
+//Tieteenkatu 3B//
+if (localStorage.Tieteenkatu3B === "true")
+{
+  document.getElementById('Tieteenkatu3B_green').style.display='block';
+}
+else {
+  document.getElementById('Tieteenkatu3B_green').style.display='none';
+}
+
+button_Tieteenkatu3B_green.onclick = function functionTieteenkatu3B_green(){
+  var pic = "green/Tieteenkatu3B_green";
+  document.getElementById('Tieteenkatu3B_green').style.display='block';
+  localStorage.Tieteenkatu3B = true;
+};
+
+button_Tieteenkatu3B_red.onclick = function functionTieteenkatu3B_red(){
+  var pic = "green/Tieteenkatu3B_green";
+  document.getElementById('Tieteenkatu3B_green').style.display='none';
+  localStorage.Tieteenkatu3B = false;
+};
+
+//More//
+
+buttonMore.onclick = function functionMore(){
+  document.getElementById('more').style.display='block';
+  document.getElementById('less').style.display='none';
+};
+
+//Less//
+
+buttonLess.onclick = function functionLess(){
+  document.getElementById('more').style.display='none';
+  document.getElementById('less').style.display='block';
+};
+
+//Reset//
+
+buttonReset.onclick = function functionReset(){
+    localStorage.clear();
+    location.reload(true);
+};
